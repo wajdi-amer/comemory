1105c9c475c39e0a1944b1f8a81028227a77d60a
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-09-25 02:29:59 +0300

    connect to mongoDb atlas

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config()
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -9,3 +11,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
