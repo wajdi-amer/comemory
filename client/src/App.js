fc7b5bf0fc18f66fa86e55e8ecafa88d13f4a2a0
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-10-11 00:33:52 +0300

    front end: create basic form and post/s components with basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+
+import useStyles from './styles';
+import comemoryIcon from './images/comemory_icon.png';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>CoMemory</Typography>
+                <img className={classes.image} src={comemoryIcon} alt='comemory icon' height='60' />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
