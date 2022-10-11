5935420d88758a3fd6dde90f7e6f81ad524f9cf4
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-10-12 00:37:07 +0300

    small fixes and mobile responsiveness

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -29,7 +29,7 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant='h5' gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant='h6' gutterBottom>{post.message}</Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
