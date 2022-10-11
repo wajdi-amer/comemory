8b7a85b81df5d9adbbf671327fe88eb4f7679bae
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-10-11 23:27:34 +0300

    editing a memory works

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,7 +4,7 @@ import Post from './Post/Post';
 import useStyles from './styles';
 import { Grid, CircularProgress } from '@material-ui/core'
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
@@ -15,7 +15,7 @@ const Posts = () => {
             <Grid className={classes.container} container alignItems='stretch' spacing={3}>
                 {posts.map((post) => (
                     <Grid item key={post._id} xs={12} sm={6}>
-                        <Post post={post} />
+                        <Post post={post} setCurrentId={setCurrentId} />
                     </Grid>
                 ))}
             </Grid>
