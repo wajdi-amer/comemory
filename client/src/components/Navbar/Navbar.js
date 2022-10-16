c63d0f9298eee4f084ae633069e2fe9b9e7a0be6
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-10-16 03:28:47 +0300

    logging out works

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect} from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Toolbar, Typography, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import comemoryIcon from '../../images/comemory_icon.png';
@@ -8,14 +10,22 @@ import useStyles from './styles';
 const Navbar = () => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile'))); 
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
 
-    console.log(user);
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+
+        history.push('/');
+        setUser(null);
+    }
     
     useEffect(() => {
         const token = user?.token;
 
         setUser(JSON.parse(localStorage.getItem('profile')))
-    }, [])
+    }, [location])
 
     return (
         <AppBar className={classes.appBar} position='static' color='inherit'>
@@ -28,7 +38,7 @@ const Navbar = () => {
                     <div className={classes.profile}>
                         <Avatar className={classes.purple} alt={user.result.name} src={user.result.picture}>{user.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
-                        <Button variant='contained' className={classes.logout} color='secondary' onClick={() => {}}>Logout</Button>
+                        <Button variant='contained' className={classes.logout} color='secondary' onClick={logout}>Logout</Button>
                     </div>) : (
                         <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
                 )}
