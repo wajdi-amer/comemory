aa5e7a18785fa5654f7c9e53bfc7bf8972a521d9
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-10-20 01:35:45 +0300

    made message input larger

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -58,7 +58,7 @@ const Form = ({ currentId, setCurrentId }) => {
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a Memory...</Typography>
                 <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-                <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+                <TextField name='message' multiline rows={4} variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
                 <TextField name='tags' variant='outlined' label='Tags (coma seperated)' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
                 <div className={classes.fileInput}>
                     <FileBase type='file' multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} />
