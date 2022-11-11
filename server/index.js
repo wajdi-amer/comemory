50d04d6da4f26fe20ef00a89aba9b9286d8ad884
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-11-12 00:44:38 +0200

    test;

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING.');
+})
+
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
